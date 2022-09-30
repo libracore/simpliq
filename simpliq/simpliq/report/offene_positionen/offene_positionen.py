d1d2004f759b34ef07a4f0784f5aad302b5a7fdf
Jonathan Furrer <jonathan@simpliq.ch>
2022-09-30 22:51:59 +0200

    added employee name to remarks / formatet date

--- simpliq/simpliq/report/offene_positionen/offene_positionen.py
+++ simpliq/simpliq/report/offene_positionen/offene_positionen.py
@@ -120,6 +120,7 @@ def get_invoiceable_entries(from_date=None, to_date=None, customer=None):
             `tabDelivery Note`.`posting_date` AS `date`,
             "Delivery Note" AS `dt`,
             `tabDelivery Note`.`name` AS `reference`,
+            NULL AS `employee_name`,
             `tabDelivery Note Item`.`name` AS `detail`,
             `tabDelivery Note`.`project` AS `project`,
             `tabDelivery Note Item`.`item_code` AS `item`,
@@ -146,6 +147,7 @@ def get_invoiceable_entries(from_date=None, to_date=None, customer=None):
             `tabAbo`.`start_date` AS `date`,
             "Abo" AS `dt`,
             `tabAbo`.`name` AS `reference`,
+            NULL AS `employee_name`,
             `tabAbo Item`.`name` AS `detail`,
             NULL AS `project`,
             `tabAbo Item`.`item` AS `item`,
@@ -187,12 +189,18 @@ def create_invoice(from_date, to_date, customer):
     })
     
     for e in entries:
+        #Format Remarks 
+        if e.remarks:
+            remarkstring = "{0} : {1} : {2}".format(e.date.strftime("%d.%m.%Y"), e.employee_name, e.remarks)
+        else:
+            remarkstring = "{0} : {1}".format(e.date.strftime("%d.%m.%Y"), e.employee_name)
+
         item = {
             'item_code': e.item,
             'qty': e.qty,
             'rate': e.rate,
             'description': e.remarks,            # will be overwritten by frappe
-            'remarks': "{0}: {1} : {2}".format(e.date, e.employee_name, e.remarks) # if e.remarks else "{0}".format(e.date)
+            'remarks': remarkstring
         }
         if e.dt == "Delivery Note":
             item['delivery_note'] = e.reference
